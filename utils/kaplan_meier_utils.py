4ea62b1b08a01d13aba602bd8d5bf31113feffc8
Matthew Leichter <matthew.leichter@gmail.com>
2025-09-23 18:43:00 +0200

    Update kaplan_meier_utils.py

--- utils/kaplan_meier_utils.py
+++ utils/kaplan_meier_utils.py
@@ -1,7 +1,16 @@
 import pandas as pd
 from lifelines import KaplanMeierFitter
 
-def compute_survival(data):
+def compute_km_curve(data):
+    """
+    Computes Kaplan-Meier survival curve.
+
+    Parameters:
+        data (pd.DataFrame): Must contain 'retention_time' and 'churned' columns.
+
+    Returns:
+        KaplanMeierFitter object
+    """
     kmf = KaplanMeierFitter()
-    kmf.fit(durations=data['tenure'], event_observed=data['churned'])
+    kmf.fit(durations=data["retention_time"], event_observed=data["churned"])
     return kmf

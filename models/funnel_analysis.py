435f54d436055cb04edbe35099552eb3ddee3c13
Matthew Leichter <matthew.leichter@gmail.com>
2025-09-23 16:35:24 +0200

    Delete models/funnel_analysis.py

--- models/funnel_analysis.py
+++
@@ -1,8 +0,0 @@
-def funnel_conversion_rate(funnel_df):
-    stages = funnel_df.columns
-    conversion_rates = {}
-    for i in range(len(stages)-1):
-        conversion_rates[f"{stages[i]}→{stages[i+1]}"] = (
-            funnel_df[stages[i+1]].sum() / funnel_df[stages[i]].sum()
-        )
-    return conversion_rates

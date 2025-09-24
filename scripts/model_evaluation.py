8d34ea3d15a7b1cf8215539552750a7d20a48e62
Matthew Leichter <matthew.leichter@gmail.com>
2025-09-24 13:04:46 +0200

    Delete scripts/model_evaluation.py

--- scripts/model_evaluation.py
+++
@@ -1,9 +0,0 @@
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-
-def evaluate_classification(y_true, y_pred):
-    return {
-        "accuracy": accuracy_score(y_true, y_pred),
-        "precision": precision_score(y_true, y_pred, average='macro'),
-        "recall": recall_score(y_true, y_pred, average='macro')
-    }
